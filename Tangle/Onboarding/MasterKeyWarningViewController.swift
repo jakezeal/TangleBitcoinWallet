c3e2b5a74cf8ebc94b9157b44ed5b5fe32aa7abe
Jake Zeal <jake@jakezeal.com>
2016-08-16 12:19:50 -0700

    add documentation to MasterKeyWarningViewController

--- Tangle/Onboarding/MasterKeyWarningViewController.swift
+++ Tangle/Onboarding/MasterKeyWarningViewController.swift
@@ -14,6 +14,11 @@ class MasterKeyWarningViewController: UIViewController {
     @IBOutlet weak var displayMasterKeyButton: UIButton!
     
     // MARK: - IBActions
+    /**
+     @name  displayMasterKeyButtonTapped
+     
+     - parameter sender: AnyObject
+     */
     @IBAction func displayMasterKeyButtonTapped(sender: AnyObject) {
         print("Display Master Key Button Tapped -> Generate Master Key View Controller \n")
         transitionToGenerateMasterKeyViewController()
@@ -21,17 +26,26 @@ class MasterKeyWarningViewController: UIViewController {
     }
     
     // MARK: - View Lifecycles
+    /**
+     @name  viewDidLoad
+     */
     override func viewDidLoad() {
         super.viewDidLoad()
         prepareDisplayMasterKeyButton()
     }
     
     // MARK: - Preparations
+    /**
+     @name  prepareDisplayMasterKeyButton
+     */
     func prepareDisplayMasterKeyButton() {
         displayMasterKeyButton.layer.cornerRadius = 5.0
     }
     
     // MARK: - Helper Methods
+    /**
+     @name  transitionToGenerateMasterKeyViewController
+     */
     func transitionToGenerateMasterKeyViewController() {
         let storyboard = UIStoryboard(name: "CreateWallet", bundle: nil)
         let generateMasterKeyViewController = storyboard.instantiateViewControllerWithIdentifier("GenerateMasterKeyViewController") as! GenerateMasterKeyViewController
